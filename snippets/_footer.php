5066259c0ba12d1ea3d3c8369070858376895ec8
Ronak Khandelwal <52578561+ronakkhandlwl@users.noreply.github.com>
2021-11-18 19:55:44 +0530

    Add files via upload

---
+++ snippets/_footer.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<div class="container col-8 bg-light footer py-2 pb-3 mt-4" id="footer">
+	<p class="text-center mb-0">
+		Copyright Discuss Software &#169; 2021 | All rights reserved
+	</p>
+	<script>
+		if ( window.history.replaceState ) {
+			window.history.replaceState( null, null, window.location.href );
+		}
+	</script>
+</div>
